a702dffb18c233fe4d7d604281815d1ad425c289
mateusz-uran <mateusz.uranowski@onet.pl>
2022-06-25 16:40:45 +0200

    Add modal for products images

--- script.js
+++ script.js
@@ -71,3 +71,29 @@ window.onscroll = () => {
     }
 }
 };
+
+/* modal image */
+const images = document.querySelectorAll(".product-img img");
+let imgIndex
+let imgSrc;
+
+images.forEach((img, i) => {
+    img.addEventListener("click", (e) => {
+        imgSrc = e.target.src;
+        imgModal(imgSrc);
+        imgIndex = i;
+    });
+});
+let imgModal = (src) => {
+    const modal = document.createElement("div");
+    modal.setAttribute("class", "modal");
+    document.querySelector(".products").append(modal);
+    const newImage = document.createElement("img");
+    newImage.setAttribute("src", src);
+    const closeBtn = document.createElement("i");
+    closeBtn.setAttribute("class", "fas fa-times closeBtn");
+    closeBtn.onclick = () => {
+        modal.remove();
+    };
+modal.append(newImage, closeBtn);
+};
