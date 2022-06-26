f8f203ff73958359e61a6e4b361d9c279f203d0b
mateusz-uran <mateusz.uranowski@onet.pl>
2022-06-26 17:26:27 +0200

    Change php script

--- mail.php
+++ mail.php
@@ -1,18 +1,40 @@
-<?php 
-if(isset($_POST['submit'])){
-  $to = "mateusz.uranowski@onet.pl"; // this is your Email address
-  $from = $_POST['email']; // this is the sender's Email address
-  $first_name = $_POST['first_name'];
-  $subject = "Form submission";
-  $subject2 = "Copy of your form submission";
-  $message = $first_name . " " . " wrote the following:" . "\n\n" . $_POST['message'];
-  $message2 = "Here is a copy of your message " . $first_name . "\n\n" . $_POST['message'];
-
-  $headers = "From:" . $from;
-  $headers2 = "From:" . $to;
-  mail($to,$subject,$message,$headers);
-  mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
-  echo "Mail Sent. Thank you " . $first_name . ", we will contact you shortly.";
-  // You can also use header('Location: thank_you.php'); to redirect to another page.
+<?php  
+ 
+if(isset($_POST['submit'])) {
+ $mailto = "mateusz.uranowski@onet.pl";  //My email address
+ //getting customer data
+ $name = $_POST['name']; //getting customer name
+ $fromEmail = $_POST['email']; //getting customer email
+ $subject = $_POST['message']; //getting subject line from client
+ $subject2 = "Confirmation: Message was submitted successfully | HMA WebDesign"; // For customer confirmation
+ 
+ //Email body I will receive
+ $message = "Cleint Name: " . $name . "\n"
+ . "Client Message: " . "\n" . $_POST['message'];
+ 
+ //Message for client confirmation
+ $message2 = "Dear" . $name . "\n"
+ . "Thank you for contacting us. We will get back to you shortly!" . "\n\n"
+ . "You submitted the following message: " . "\n" . $_POST['message'] . "\n\n"
+ . "Regards," . "\n" . "- HMA WebDesign";
+ 
+ //Email headers
+ $headers = "From: " . $fromEmail; // Client email, I will receive
+ $headers2 = "From: " . $mailto; // This will receive client
+ 
+ //PHP mailer function
+ 
+  $result1 = mail($mailto, $subject, $message, $headers); // This email sent to My address
+  $result2 = mail($fromEmail, $subject2, $message2, $headers2); //This confirmation email to client
+ 
+  //Checking if Mails sent successfully
+ 
+  if ($result1 && $result2) {
+    $success = "Your Message was sent Successfully!";
+  } else {
+    $failed = "Sorry! Message was not sent, Try again Later.";
   }
+ 
+}
+ 
 ?>
